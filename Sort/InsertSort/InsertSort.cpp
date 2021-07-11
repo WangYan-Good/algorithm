abaeb379b5322e2ce3b1a4bdb12d8a2320b9df5d
WangYan-Good <987639823@qq.com>
2021-07-12 01:04:02 +0800

    algorithm_InsertSort

--- Sort/InsertSort/InsertSort.cpp
+++ Sort/InsertSort/InsertSort.cpp
@@ -4,22 +4,21 @@
 template <typename Type>
 void insert_sort( std::vector<Type> & array, int length )
 {
-    Type key;
-    if( array.empty() || array.size() == 1 )
+    if( array.empty() || length == 1 )
     {
         return;
     }
 
-    for( int j = 2; j < array.size(); j++ )
+    for( int j = 2; j < length; j++ )
     {
-        key = array[j];
+        array[0] = array[j];
         // Insert array[j] into the sorted sequence array[1...j-1]
         int i = j -1;
-        while( i > 0 && array[i] > key )
+        while( i > 0 && array[i] > array[0] )
         {
             array[i+1] = array[i];
             i -= 1;
         }
-        array[i+1] = key;
+        array[i+1] = array[0];
     }
 }
