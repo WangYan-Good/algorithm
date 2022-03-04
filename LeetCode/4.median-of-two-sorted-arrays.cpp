ed7bae3a2f258d27e0a125f218845e5d09c773d6
Yan Yan89 Wang <wangyan89@lenovo.com>
2022-03-04 17:48:48 +0800

    algorithm_leetcode_4

--- LeetCode/4.median-of-two-sorted-arrays.cpp
+++ LeetCode/4.median-of-two-sorted-arrays.cpp
@@ -8,73 +8,96 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int size = nums1.size() + nums2.size();
-        double pre_median;
-        double median;
-        bool flag;
-        int index = 0;
 
-        // flag = true odd number
-        // flag = false even number
-        if (size % 2 ==0) {
-            flag = false;
+        // //
+        // // solution 1
+        // //
+        // int size = nums1.size() + nums2.size();
+        // double pre_median;
+        // double median;
+        // bool flag;
+        // int index = 0;
+
+        // // flag = true odd number
+        // // flag = false even number
+        // if (size % 2 ==0) {
+        //     flag = false;
+        // } else {
+        //     flag = true;
+        // }
+
+        // // initial median
+        // if (nums1.empty() && nums2.empty()) { // nums1 is empty & nums2 is empty
+        //     return 0;
+        // }
+
+        // if (nums1.empty() && !nums2.empty()) { // nums1 is empty & nums2 is not empty
+        //     if (flag) {
+        //         return (double)nums2[size/2];
+        //     } else {
+        //         return ((double)(nums2[size/2]) + (double)(nums2[size/2 - 1]));
+        //     }
+        // }
+
+        // if (nums2.empty() && !nums1.empty()) { // nums2 is empty & nums1 is not empty
+        //     if (flag) {
+        //         return (double)nums1[size/2];
+        //     } else {
+        //         return ((double)(nums1[size/2]) + (double)(nums1[size/2 - 1]));
+        //     }
+        // }
+
+        // if (!nums1.empty() && !nums2.empty()) { // nums1 is not empty & nums2 is not empty
+        //     // median = nums1[0] < nums2[0] ? nums1[0] : nums2[0];
+        //     vector<int>::iterator inums1 = nums1.begin();
+        //     vector<int>::iterator inums2 = nums2.begin();
+
+        //     // initial median
+        //     pre_median = 0;
+        //     median = (*inums1 <= *inums2) ? *inums1 : *inums2;
+        //     while (index < size/2) {
+        //         index ++;
+        //         if (*inums1 <= *inums2) {
+        //             if (inums1 < nums1.end()) {
+        //                 inums1++;
+        //             }
+        //         } else {
+        //             if (inums2 < nums2.end()) {
+        //                 inums2++;
+        //             }
+        //         }
+        //         pre_median = median;
+        //         median = (*inums1 <= *inums2)?(double)(*inums1):(double)(*inums2);
+        //     }
+        //     if (!flag) {
+        //         median = (median + pre_median)/2;
+        //     }
+        //     return median;
+        // }
+        // return 0;
+
+        //
+        // solution 2
+        //
+        int size = nums1.size () + nums2.size ();
+        vector<int> merge_arr (size);
+
+        for (auto x : nums1) merge_arr.push_back (x);
+        for (auto x : nums2) merge_arr.push_back (x);
+
+        sort (merge_arr.begin (), merge_arr.end ());
+        
+        if (size % 2 == 0) {
+            return (merge_arr[size/2 - 1] + merge_arr[size/2])/2;
         } else {
-            flag = true;
+            return merge_arr[size/2];
         }
-
-        // initial median
-        if (nums1.empty() && nums2.empty()) { // nums1 is empty & nums2 is empty
-            return 0;
-        }
-
-        if (nums1.empty() && !nums2.empty()) { // nums1 is empty & nums2 is not empty
-            if (flag) {
-                return (double)nums2[size/2];
-            } else {
-                return ((double)(nums2[size/2]) + (double)(nums2[size/2 - 1]));
-            }
-        }
-
-        if (nums2.empty() && !nums1.empty()) { // nums2 is empty & nums1 is not empty
-            if (flag) {
-                return (double)nums1[size/2];
-            } else {
-                return ((double)(nums1[size/2]) + (double)(nums1[size/2 - 1]));
-            }
-        }
-
-        if (!nums1.empty() && !nums2.empty()) { // nums1 is not empty & nums2 is not empty
-            // median = nums1[0] < nums2[0] ? nums1[0] : nums2[0];
-            vector<int>::iterator inums1 = nums1.begin();
-            vector<int>::iterator inums2 = nums2.begin();
-
-            // initial median
-            pre_median = 0;
-            median = (*inums1 <= *inums2) ? *inums1 : *inums2;
-            while (index < size/2) {
-                index ++;
-                if (*inums1 <= *inums2) {
-                    if (inums1 < nums1.end()) {
-                        inums1++;
-                    }
-                } else {
-                    if (inums2 < nums2.end()) {
-                        inums2++;
-                    }
-                }
-                pre_median = median;
-                median = (*inums1 <= *inums2)?(double)(*inums1):(double)(*inums2);
-            }
-            if (!flag) {
-                median = (median + pre_median)/2;
-            }
-            return median;
-        }
-        return 0;
+        
     }
 };
 // @lc code=end
