a79f7087b73f5f62654a47a5ce6edc345c0977b1
WangYan-Good <987639823@qq.com>
2022-05-20 17:24:57 +0800

    algorithm_leetcode_medium_11

--- LeetCode/11.container-with-most-water.cpp
+++ LeetCode/11.container-with-most-water.cpp
@@ -7,17 +7,39 @@
 // @lc code=start
 #include <iostream>
 #include <vector>
+#include <algorithm>
+// #include <stack>
 using namespace std;
+
+// typedef struct {
+//     int value;
+//     int index;
+// } KeyValue;
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-       int left = 0;
-       int right = height.size() - 1;
-       int most_water = ( right - left ) * min( height[left], height[right] );
+        int left, right, max, temp, high, width;
+        left = 0;
+        right = height.size()-1;
+        max = 0;
+        
+        while (left < right) {
+            width = right - left;
+            if (height[left] < height[right]) {
+                high = height[left];
+                left++;
+            } else {
+                high = height[right];
+                right--;
+            }
 
-       while ( left < right ) {
-           
-       }
+            temp = width*high;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return max;
     }
 };
 // @lc code=end
